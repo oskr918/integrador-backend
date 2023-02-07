996fb04165f7692584c98660b8d70dcf606d9aa8
Aleli7 <queenveryhappy.mode@gmail.com>
2023-02-06 23:22:44 -0300

    Updates

---
+++ index.js
@@ -0,0 +1,25 @@
+require("rootpath")();
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const config = require("./src/config/config.json");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('tiny'));
+morgan(':method :url :status :res[content-length] - :response-time ms');
+  
+app.get("/", function (req, res) {
+    res.send("Bienvenido");
+});
+
+const alumnoCont = require("./src/controller/alumnoController.js");
+app.use("/api/alumno", alumnoCont);
+
+app.listen(config.server.port, function (err) {
+  if (err) {
+      console.log(err);
+  } else {
+      console.log(`Server iniciado en puerto:${config.server.port}`);
+  }
+});
