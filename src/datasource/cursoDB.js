b0e22e543ac294a3a4e1cb6fd458ab5c19901b0a
Aleli7 <queenveryhappy.mode@gmail.com>
2023-02-19 02:26:02 -0300

    Configuraciones

--- src/datasource/cursoDB.js
+++ src/datasource/cursoDB.js
@@ -51,7 +51,7 @@ cursoDB.getAlumnosByIdCurso = function (id, funCallBack) {
 }
 
 cursoDB.createCurso = function (curso, funCallBack) {
-   var query = "INSERT INTO curso (id, nombre, descripcion, imagen, anio, activo) VALUES (?, ?, ?, ?, ?, ?)";
+   var query = 'INSERT INTO curso (id, nombre, descripcion, imagen, anio, activo) VALUES (?, ?, ?, ?, ?, ?)';
    var dbParams = [curso.id, curso.nombre, curso.descripcion, curso.imagen, curso.anio, curso.activo];
    connection.query(query, dbParams, function (err, result, fields) {
       if (err) {
@@ -69,7 +69,7 @@ cursoDB.createCurso = function (curso, funCallBack) {
          console.error(err);
       } else {
          funCallBack({
-            message: `Se ceo el curso ${curso.nombre}`,
+            message: `Se creó el curso ${curso.nombre}`,
             detail: result
          });
       }
@@ -77,7 +77,7 @@ cursoDB.createCurso = function (curso, funCallBack) {
 }
 
 cursoDB.updateCurso = function (id, curso, funCallBack) {
-   var query = "UPDATE curso SET id = ?, nombre = ?, descripcion = ?, imagen = ?, anio = ?, activo = ? WHERE id = ? "
+   var query = 'UPDATE curso SET id = ?, nombre = ?, descripcion = ?, imagen = ?, anio = ?, activo = ? WHERE id = ?'
    var dbParams = [curso.id, curso.nombre, curso.descripcion, curso.imagen, curso.anio, curso.activo, id];
    connection.query(query, dbParams, function (err, result, fields) {
       if (err) {
